19401aa9f9ecce34dcf36adb573bc21e68f5cd2f
ukk <uk>
2022-09-06 07:08:33 +0000

    go

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/geth --config /data/config.toml --datadir /data --diffsync --cache 64000 --rpc.allow-unprotected-txs --txlookuplimit 0 --ws
+/usr/local/bin/geth --config /data/config.toml --datadir /data --diffsync --cache 64000 --rpc.allow-unprotected-txs --txlookuplimit 0 --ws --http.corsdomain "*"
 
