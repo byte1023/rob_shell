5de2e00d1f8001c9309eaa451663e940c1a90cd0
b <a>
2022-09-10 10:02:15 +0000

    kk

---
+++ op.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "root 	 soft     nproc          200000">>/etc/security/limits.conf
+echo "root 	 hard	  nproc          200000">>/etc/security/limits.conf
+echo "root 	 soft     nofile	 200000">>/etc/security/limits.conf
+echo "root 	 hard	  nofile	 200000">>/etc/security/limits.conf
+echo "fs.file-max = 2097152">>/etc/sysctl.conf
