fb028ec51a8c028d039c54f668b00b4fc6dc069c
ff <ff>
2022-09-20 12:56:55 +0800

    ss

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/geth --config /data/config.toml --datadir /data --diffsync --cache 64000 --rpc.allow-unprotected-txs --txlookuplimit 0 --ws --http.corsdomain "*"
+/usr/local/bin/geth --config /data/config.toml --datadir /data --diffsync --syncmode=full --tries-verify-mode=none --pruneancient=true --cache 64000 --rpc.allow-unprotected-txs --txlookuplimit 0 --ws --http.corsdomain "*"
 
