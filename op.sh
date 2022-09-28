185aba5411f5877ef1651b5b73a863b0a7acc5f0
lee@asburn <lee@asburn>
2022-09-29 03:12:12 +0800

    1

--- op.sh
+++ op.sh
@@ -4,3 +4,5 @@ echo "root 	 hard	  nproc          200000">>/etc/security/limits.conf
 echo "root 	 soft     nofile	 200000">>/etc/security/limits.conf
 echo "root 	 hard	  nofile	 200000">>/etc/security/limits.conf
 echo "fs.file-max = 2097152">>/etc/sysctl.conf
+sysctl -p
+timedatectl set-timezone Asia/Shanghai
