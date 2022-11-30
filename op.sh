aa22ab64858992095e843746dd2080eea908cdc7
y <x>
2022-11-30 23:35:10 +0800

    kk

--- op.sh
+++ op.sh
@@ -23,7 +23,7 @@ tar -zxvf ~/doc/rob_shell/log.io.config.tar.gz -C ~
 tar -zxvf ~/doc/rob_shell/ui.tar.gz -C /usr/local/node-v16.17.0-linux-x64/lib/node_modules/log.io/lib/
 ln -s /usr/local/node-v16.17.0-linux-x64/bin/log.io-server /usr/local/bin/
 ln -s /usr/local/node-v16.17.0-linux-x64/bin/log.io-file-input /usr/local/bin/
-echi '-------------------'
+echo '-------------------'
 date
 ulimit -a
 geth version
