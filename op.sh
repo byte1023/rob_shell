96102f69b0fce36f844b43fc0cc0daa432afa913
x <x>
2022-12-01 16:49:45 +0800

    xxx

--- op.sh
+++ op.sh
@@ -19,10 +19,12 @@ ln -s /usr/local/node-v16.17.0-linux-x64/bin/node /usr/local/bin/
 ln -s /usr/local/node-v16.17.0-linux-x64/bin/npm /usr/local/bin/
 npm isntall -g log.io
 npm isntall -g log.io-file-input
+npm install -g pm2
 tar -zxvf ~/doc/rob_shell/log.io.config.tar.gz -C ~
 tar -zxvf ~/doc/rob_shell/ui.tar.gz -C /usr/local/node-v16.17.0-linux-x64/lib/node_modules/log.io/lib/
 ln -s /usr/local/node-v16.17.0-linux-x64/bin/log.io-server /usr/local/bin/
 ln -s /usr/local/node-v16.17.0-linux-x64/bin/log.io-file-input /usr/local/bin/
+ln -s /usr/local/node-v16.17.0-linux-x64/bin/pm2 /usr/local/bin/
 echo '-------------------'
 date
 ulimit -a
